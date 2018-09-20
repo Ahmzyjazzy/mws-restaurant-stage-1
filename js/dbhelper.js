abdb5597192a622f7ff49c7c00507b4fb3dfb917
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2018-09-20 22:04:42 +0100

    feat: fetch restuarants from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,18 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 4000; // Change this to your server port
-    return `${window.location.origin}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `${window.location.origin}:${port}`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch('http://127.0.0.1:1337/restaurants')
+    .then(response => response.json())
+    .then( restaurants => callback(null, restaurants))
+    .catch( err => callback(error.message, null));
   }
 
   /**
